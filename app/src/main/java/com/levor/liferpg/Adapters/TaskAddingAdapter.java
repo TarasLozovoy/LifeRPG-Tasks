8342ff5bca7e0247c07a123d08aa164b1c03002c
Taras Lozovoy <tlozovyi@intropro.com>
2015-10-29 15:30:46 +0200

    improved adding of tasks

---
+++ app/src/main/java/com/levor/liferpg/Adapters/TaskAddingAdapter.java
@@ -0,0 +1,62 @@
+package com.levor.liferpg.Adapters;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ListAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.levor.liferpg.Controller.LifeController;
+import com.levor.liferpg.Model.Skill;
+import com.levor.liferpg.Model.Task;
+import com.levor.liferpg.R;
+
+import java.util.List;
+
+/**
+ * Created by Levor on 10/29/15.
+ */
+public class TaskAddingAdapter extends BaseAdapter implements ListAdapter {
+    private Context mContext;
+    protected List<String> items;
+
+
+    public TaskAddingAdapter(Context context, List<String> array){
+        this.mContext = context;
+        this.items = array;
+    }
+
+    @Override
+    public int getCount() {
+        return items.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return items.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        View view = convertView;
+        if (view == null){
+            LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            view = inflater.inflate(R.layout.task_add_list_view, null);
+        }
+
+        TextView listItemText = (TextView) view.findViewById(R.id.list_item_string);
+        listItemText.setText(items.get(position));
+        return view;
+    }
+}
--- app/src/main/java/com/levor/liferpg/View/AddTaskActivity.java
+++ app/src/main/java/com/levor/liferpg/View/AddTaskActivity.java
@@ -4,17 +4,17 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListAdapter;
 import android.widget.ListView;
-import android.widget.PopupMenu;
-import android.widget.TextView;
 import android.widget.Toast;
 
+import com.levor.liferpg.Adapters.TaskAddingAdapter;
 import com.levor.liferpg.Controller.LifeController;
 import com.levor.liferpg.R;
 
@@ -22,11 +22,11 @@ import java.util.ArrayList;
 
 public class AddTaskActivity extends AppCompatActivity {
     private EditText newTaskTitleEditText;
-    private TextView relatedSkillToAdd;
+    private ListView relatedSkillListView;
     private Button addSkillButton;
     private Button finishActivity;
 
-    private ArrayList<String> relatedskills = new ArrayList<>();
+    private ArrayList<String> relatedSkills = new ArrayList<>();
 
     private final LifeController lifeController = LifeController.getInstance();
 
@@ -35,16 +35,27 @@ public class AddTaskActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_task);
         newTaskTitleEditText = (EditText) findViewById(R.id.new_task_title_edit_text);
-        relatedSkillToAdd = (TextView) findViewById(R.id.related_skills_to_add);
+
+        relatedSkillListView = (ListView) findViewById(R.id.related_skills_to_add);
+        relatedSkillListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                relatedSkills.remove(position);
+                updateListView();
+            }
+        });
+        updateListView();
+
         finishActivity = (Button) findViewById(R.id.finish_activity);
         finishActivity.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 setResult(RESULT_OK);
-                lifeController.createNewTask(newTaskTitleEditText.getText().toString(), relatedskills);
+                lifeController.createNewTask(newTaskTitleEditText.getText().toString(), relatedSkills);
                 AddTaskActivity.this.finish();
             }
         });
+
         addSkillButton = (Button) findViewById(R.id.add_related_skill);
         addSkillButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -57,14 +68,20 @@ public class AddTaskActivity extends AppCompatActivity {
                 dialog.setAdapter(adapter, new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        relatedskills.add(adapter.getItem(which));
-                        relatedSkillToAdd.setText(relatedSkillToAdd.getText() + adapter.getItem(which) + " ");
+                        if (!relatedSkills.contains(adapter.getItem(which))) {
+                            relatedSkills.add(adapter.getItem(which));
+                            updateListView();
+                        }
                         dialog.dismiss();
                     }
                 });
                 dialog.show();
             }
         });
+    }
+
+    private void updateListView(){
+        relatedSkillListView.setAdapter(new TaskAddingAdapter(this, relatedSkills));
     }
 
     @Override
