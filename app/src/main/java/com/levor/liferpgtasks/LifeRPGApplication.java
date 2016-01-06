4e03a549faa7a249e641bcaf1ca427b92c4d402d
TarasLozovoy <jiebop@mail.ru>
2016-01-06 11:21:16 +0200

    Started addin analytics

--- app/src/main/java/com/levor/liferpgtasks/LifeRPGApplication.java
+++ app/src/main/java/com/levor/liferpgtasks/LifeRPGApplication.java
@@ -4,6 +4,8 @@ import android.app.Application;
 
 import com.crashlytics.android.Crashlytics;
 import com.facebook.FacebookSdk;
+import com.google.android.gms.analytics.GoogleAnalytics;
+import com.google.android.gms.analytics.Tracker;
 import com.twitter.sdk.android.core.TwitterAuthConfig;
 import com.twitter.sdk.android.core.TwitterCore;
 import com.twitter.sdk.android.tweetcomposer.TweetComposer;
@@ -12,6 +14,7 @@ import com.vk.sdk.VKSdk;
 import io.fabric.sdk.android.Fabric;
 
 public class LifeRPGApplication extends Application {
+    private Tracker mTracker;
 
     @Override
     public void onCreate() {
@@ -25,5 +28,13 @@ public class LifeRPGApplication extends Application {
 
         TwitterAuthConfig authConfig =  new TwitterAuthConfig("w4GMxIA6anN7qWmqgyGFGGd2r", "skRyGWmGKYzJBWQrflywymbH6NYpdxHtV2JijiRKRyILANrQLp");
         Fabric.with(this, new TwitterCore(authConfig), new TweetComposer(), new Crashlytics());
+    }
+
+    synchronized public Tracker getDefaultTracker() {
+        if (mTracker == null) {
+            GoogleAnalytics analytics = GoogleAnalytics.getInstance(this);
+            mTracker = analytics.newTracker(R.xml.global_tracker);
+        }
+        return mTracker;
     }
 }
