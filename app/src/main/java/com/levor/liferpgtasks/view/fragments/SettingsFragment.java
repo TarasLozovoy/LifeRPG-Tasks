1a127a38a4fcf5e50a76d583e984deba30ad5a54
Taras Lozovoy <tlozovyi@intropro.com>
2016-01-18 11:50:08 +0200

    Added contact button.

--- app/src/main/java/com/levor/liferpgtasks/view/fragments/SettingsFragment.java
+++ app/src/main/java/com/levor/liferpgtasks/view/fragments/SettingsFragment.java
@@ -1,6 +1,9 @@
 package com.levor.liferpgtasks.view.fragments;
 
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,9 +12,12 @@ import android.widget.Button;
 
 import com.levor.liferpgtasks.R;
 
+import java.util.List;
+
 public class SettingsFragment extends DefaultFragment {
     private Button showStatisticsButton;
     private Button showAchievementsButton;
+    private Button contactButton;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -19,6 +25,7 @@ public class SettingsFragment extends DefaultFragment {
         View v = inflater.inflate(R.layout.fragment_settings, container, false);
         showStatisticsButton = (Button) v.findViewById(R.id.show_statistics_button);
         showAchievementsButton = (Button) v.findViewById(R.id.show_achievements_button);
+        contactButton = (Button) v.findViewById(R.id.contact_button);
 
         showStatisticsButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,6 +38,24 @@ public class SettingsFragment extends DefaultFragment {
             @Override
             public void onClick(View v) {
                 getCurrentActivity().showChildFragment(new AchievementsFragment(), null);
+            }
+        });
+
+        contactButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent emailIntent = new Intent(Intent.ACTION_SEND);
+                emailIntent.putExtra(Intent.EXTRA_EMAIL, new String[]{"liferpgtasks@gmail.com"});
+                emailIntent.setType("text/plain");
+                final PackageManager pm = getCurrentActivity().getPackageManager();
+                final List<ResolveInfo> matches = pm.queryIntentActivities(emailIntent, 0);
+                ResolveInfo best = null;
+                for(final ResolveInfo info : matches)
+                    if (info.activityInfo.packageName.endsWith(".gm") || info.activityInfo.name.toLowerCase().contains("gmail"))
+                        best = info;
+                if (best != null)
+                    emailIntent.setClassName(best.activityInfo.packageName, best.activityInfo.name);
+                getCurrentActivity().startActivity(emailIntent);
             }
         });
 
