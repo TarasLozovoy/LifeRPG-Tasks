4e6f08618f9a7b5f924c3dfe9617183d3586f7d1
TarasLozovoy <jiebop@mail.ru>
2016-04-02 13:25:40 +0300

    Added possibility to select displayable characteristics in characteristics chart.

--- app/src/main/java/com/levor/liferpgtasks/view/fragments/characteristics/CharacteristicsChartFragment.java
+++ app/src/main/java/com/levor/liferpgtasks/view/fragments/characteristics/CharacteristicsChartFragment.java
@@ -3,6 +3,8 @@ package com.levor.liferpgtasks.view.fragments.characteristics;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,11 +13,10 @@ import com.github.mikephil.charting.charts.RadarChart;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.RadarData;
 import com.github.mikephil.charting.data.RadarDataSet;
-import com.google.android.gms.ads.AdListener;
-import com.google.android.gms.ads.AdView;
 import com.levor.liferpgtasks.R;
 
 import com.levor.liferpgtasks.model.Characteristic;
+import com.levor.liferpgtasks.view.Dialogs.KeyCharacteristicsSelectionDialog;
 import com.levor.liferpgtasks.view.activities.MainActivity;
 import com.levor.liferpgtasks.view.fragments.DefaultFragment;
 
@@ -29,13 +30,17 @@ import butterknife.ButterKnife;
 public class CharacteristicsChartFragment extends DefaultFragment{
     @Bind(R.id.radar_chart)     RadarChart radarChart;
 
+
+    private ArrayList<String> charTitles;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_characteristics_chart, container, false);
         ButterKnife.bind(this, view);
+        setupCharacteristicsList();
         createChart();
 
+        setHasOptionsMenu(true);
         getCurrentActivity().showActionBarHomeButtonAsBack(true);
         return view;
     }
@@ -47,11 +52,32 @@ public class CharacteristicsChartFragment extends DefaultFragment{
     }
 
     @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        menu.clear();
+        inflater.inflate(R.menu.menu_characteristics_chart_fragment, menu);
+    }
+
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case android.R.id.home:
                 getCurrentActivity().showInterstitialAd(MainActivity.AdType.CHARACTERISTICS_CHART);
                 break;
+            case R.id.edit_chart:
+                KeyCharacteristicsSelectionDialog dialog = new KeyCharacteristicsSelectionDialog();
+                Bundle b = new Bundle();
+                b.putStringArrayList(KeyCharacteristicsSelectionDialog.CHARS_LIST, charTitles);
+                dialog.setArguments(b);
+                dialog.setListener(new KeyCharacteristicsSelectionDialog.KeyCharacteristicsChangedListener() {
+                    @Override
+                    public void onChanged(ArrayList<String> charsTitles) {
+                        charTitles = charsTitles;
+                        radarChart.clear();
+                        createChart();
+                    }
+                });
+                dialog.show(getCurrentActivity().getSupportFragmentManager(), "CharacteristicsSelection");
+                return true;
         }
         return super.onOptionsItemSelected(item);
     }
@@ -63,22 +89,31 @@ public class CharacteristicsChartFragment extends DefaultFragment{
         return true;
     }
 
-    private void createChart() {
-        ArrayList<Entry> values = new ArrayList<>();
+    private void setupCharacteristicsList() {
         List<Characteristic> chars = getController().getCharacteristics();
         Collections.sort(chars, Characteristic.TITLE_COMPARATOR);
-        List<String> titles = new ArrayList<>();
+        charTitles = new ArrayList<>();
 
         for (int i = 0; i < chars.size(); i++) {
             Characteristic ch = chars.get(i);
+            charTitles.add(ch.getTitle());
+        }
+    }
+
+    private void createChart() {
+        ArrayList<Entry> values = new ArrayList<>();
+        for (int i = 0; i < charTitles.size(); i++) {
+            Characteristic ch = getController().getCharacteristicByTitle(charTitles.get(i));
             values.add(new Entry(ch.getLevel(), i));
-            titles.add(ch.getTitle());
         }
 
         RadarDataSet radarDataSet = new RadarDataSet(values, getString(R.string.characteristics_fragment_name));
-        radarDataSet.setColor(R.color.accent);
-        RadarData radarData = new RadarData(titles, radarDataSet);
+        radarDataSet.setColor(R.color.dark_grey);
+        radarDataSet.setFillColor(R.color.red);
+        radarDataSet.setDrawFilled(true);
+        RadarData radarData = new RadarData(charTitles, radarDataSet);
         radarChart.setData(radarData);
+        radarChart.setDescription("");
         radarChart.invalidate();
     }
 }
